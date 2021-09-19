d239e390c900e2489160eb6f53652a8566b5e74b
Ivan <bomak54@gmail.com>
2021-09-19 16:32:11 +0300

    created api and get request

---
+++ src/api/api.js
@@ -0,0 +1,13 @@
+const axios = require("axios");
+
+const _apiKey = `aeee6b2ec2bdfab0338f32f801a63d20`
+
+
+export const getLocation = async (_lat, _long) => {
+    try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${_lat}&lon=${_long}&appid=${_apiKey}`)
+        return response.data
+    } catch (err) {
+        console.error(err.toJSON())
+    }
+};
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./components/app";
 import ErrorBoundry from "./components/error-boundry";
+import {Provider} from "react-redux";
+import store from "./store/store";
 
 ReactDOM.render(
-    <ErrorBoundry>
-        <App/>
-    </ErrorBoundry>, document.getElementById('root')
+    <Provider store={store}>
+        <ErrorBoundry>
+            <App/>
+        </ErrorBoundry>
+    </Provider>
+   , document.getElementById('root')
 );
 
