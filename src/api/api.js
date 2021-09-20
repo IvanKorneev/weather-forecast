322dcc8258cf5a02f353b0607b930d6b1edc4920
Ivan <bomak54@gmail.com>
2021-09-20 10:15:01 +0300

    add function getWeatherCity

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,8 @@ const axios = require("axios");
 const _apiKey = `aeee6b2ec2bdfab0338f32f801a63d20`
 
 
+
+
 export const getLocation = async (_lat, _long) => {
     try {
         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${_lat}&lon=${_long}&appid=${_apiKey}`)
@@ -11,3 +13,13 @@ export const getLocation = async (_lat, _long) => {
         console.error(err.toJSON())
     }
 };
+export const getWeatherCity  = async (data)=>{
+    try {
+        const response = await  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=${_apiKey}`)
+        return response.data
+    }catch (err){
+        console.error(err.toJSON())
+    }
+}
+
+
